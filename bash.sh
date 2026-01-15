7831d37ce50ee42c266cbd38ad7600e6d9c7eaed
Tobzon <tobias19995@gmail.com>
2026-01-15 10:17:27 +0100

    felhantering

--- bash.sh
+++ bash.sh
@@ -1,71 +1,91 @@
 #!/bin/bash
 
+set -euo pipefail
 
 #genomföra minst en säkerhetskontroll, exempelvis:
-#analys av systemloggar
-#upptäckt av ovanlig nätverkstrafik
-#kontroll av installerade paket och uppdateringsstatus
-#innehålla strukturerad felhantering
-#logga output i en fil
+#analys av systemloggar ✓
+#upptäckt av ovanlig nätverkstrafik ✓
+#kontroll av installerade paket och uppdateringsstatus ✓
+#innehålla strukturerad felhantering ✓
+#logga output i en fil ✓
 #följa best practice för shellscript 
 
 
-logFile="logs"
+
+logFile="logs.log"
 
 
 
 log() {
-    echo "$(LC_TIME=sv_SE.UTF-8 date '+%F %T') -$1" | tee -a $logFile
+    local msg="$1"
+    echo "$(LC_TIME=sv_SE.UTF-8 date '+%F %T') -$msg" | tee -a $logFile
 }
 
 #listar upp alla tcp och udp sockets i numeric form, 1 per rad
 checkNetwork() {
 local threshold=100
 
-count=$(ss -ntu | wc -l)
+if ! count=$(ss -ntu 2>/dev/null | wc -l)
+    log "FEL: kunde nite läsa nästverksanslutningar"
+    return 1
 
-if [ $count -gt $threshold ]; then
+# (()) ist för [] då det är säkrare för heltal
+if (( $count -gt $threshold )); then
     log "Ovanligt många nätverksanslutningar: $count"
 fi
 }
 
 checkSsh() {
-    jounralctl -u sshd \
+
+if ! command -v journalctl >/dev/null; then
+        log "FEL: journalctl saknas"
+        return 1
+fi
+
+journalctl -u sshd \
     | grep -Ei "Accepted|Failed" \
     | awk '{print $(NF-3)}' \
-    | sort -u >> $logFile
+    | sort -u >> "$logFile" || \
+    log "FEL: kunde inte läsa ssh-loggar"
 }
 
 checkUpdates() {
-if apt list --upgradable 2>/dev/null | grep -q upgradable; then
-    log "Det finns uppdateringar"
-    sudo apt update -qq
-    
-else
-    log "Systemet är uppdaterat"
-fi
+ local updates
+
+    if ! updates=$(apt list --upgradable 2>/dev/null); then
+        log "FEL: Kunde inte kontrollera uppdateringar"
+        return 1
+    fi
+
+    echo "$updates" | grep security >> "$logFile"
+
+    if echo "$updates" | grep -q upgradable; then
+        log "Det finns uppdateringar"
+    else
+        log "Systemet är uppdaterat"
+    fi
 }
 
 fileCheck() {
 
-local file=$1
+ local file="$1"
 
-echo "Kontrollerar filrättigheter..."
-
-if [ ! -f "$file" ]; then
-    log "FEL: Filen "$file" existerar inte "
-fi 
-
-if [ -r "$file" ]; then 
-    log "filen "$file" går att läsa"
-else
-    log "FEL: Filen "$file" går inte att läsa"
+if [[ -z "$file" ]]; then
+    log "FEL: Ingen fil angiven"
+    return 1
 fi
 
-if [ -w "$file" ]; then
-    log "filen "$file" går att redigera"
-else
-    log "FEL: filen "$file" går inte att redigera"
+if [[ ! -f "$file" ]]; then
+    log "FEL: Filen $file existerar inte"
+    return 1
 fi
+
+[[ -r "$file" ]] \
+    && log "Filen $file går att läsa" \
+    || log "FEL: Filen $file går inte att läsa"
+
+[[ -w "$file" ]] \
+    && log "Filen $file går att redigera" \
+    || log "FEL: Filen $file går inte att redigera"
 }
 
