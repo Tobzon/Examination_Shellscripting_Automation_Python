e7d938a12acae5d95be922ebf34b0ca0fbd70f25
Tobzon <tobias19995@gmail.com>
2026-01-15 10:00:37 +0100

    paket och uppdateringsstatus + fixes

--- bash.sh
+++ bash.sh
@@ -2,7 +2,6 @@
 
 
 #genomföra minst en säkerhetskontroll, exempelvis:
-#kontroll av filrättigheter
 #analys av systemloggar
 #upptäckt av ovanlig nätverkstrafik
 #kontroll av installerade paket och uppdateringsstatus
@@ -19,52 +18,54 @@ log() {
     echo "$(LC_TIME=sv_SE.UTF-8 date '+%F %T') -$1" | tee -a $logFile
 }
 
-
+#listar upp alla tcp och udp sockets i numeric form, 1 per rad
 checkNetwork() {
 local threshold=100
 
-#listar upp alla tcp och udp sockets i numeric form, 1 per rad
 count=$(ss -ntu | wc -l)
 
-if((count > threshold)); then
-    log "Ovanligt många nätverksansutningar: $count"
+if [ $count -gt $threshold ]; then
+    log "Ovanligt många nätverksanslutningar: $count"
 fi
-
-jounralctl -u sshd \
-    | grep -Ei "Accepted|Failed" \
-    | awk '{print $(NF-3)}' \
-    | sort -u > -a $logFile
 }
 
+checkSsh() {
+    jounralctl -u sshd \
+    | grep -Ei "Accepted|Failed" \
+    | awk '{print $(NF-3)}' \
+    | sort -u >> $logFile
+}
 
-
-checkSystemLogs() {
-
-local logSearch="/var/log/syslog"
-local words="error|fail|critical"
-
-# hämtar logs med error,fail,critical i syslog o skriver ner dom i en ny fil, -Ei gör så att man kan göra uttryck utan backslahes(E,en slags regex användning) och den är inte case-senseitive(i) 
-grep -Ei "$words" "$logSearch" > syslogs.log
+checkUpdates() {
+if apt list --upgradable 2>/dev/null | grep -q upgradable; then
+    log "Det finns uppdateringar"
+    sudo apt update -qq
+    
+else
+    log "Systemet är uppdaterat"
+fi
 }
 
 fileCheck() {
 
 local file=$1
 
-if [ ! -f "$file"]; then
+echo "Kontrollerar filrättigheter..."
+
+if [ ! -f "$file" ]; then
     log "FEL: Filen "$file" existerar inte "
 fi 
 
-if [ -r "$file"]; then 
+if [ -r "$file" ]; then 
     log "filen "$file" går att läsa"
 else
     log "FEL: Filen "$file" går inte att läsa"
-    
 fi
 
-if [ -w "$file"]; then
+if [ -w "$file" ]; then
     log "filen "$file" går att redigera"
 else
     log "FEL: filen "$file" går inte att redigera"
 fi
 }
+
